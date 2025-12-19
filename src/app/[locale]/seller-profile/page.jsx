0f2440167694733193aae104afa0ec4124605b36
subhadeep-webart <subhadeep.das@webart.technology>
2025-12-19 17:40:50 +0530

    done

--- src/app/[locale]/seller-profile/page.jsx
+++ src/app/[locale]/seller-profile/page.jsx
@@ -1,20 +1,20 @@
-import SellerProfileBannerSection from "@/components/sellerprofile/SellerProfileBannerSection/index";
-import BestDealsSection from "@/components/sellerprofile/BestDealsSection/index";
-import CollectionSection from "@/components/sellerprofile/CollectionSection/index";
-import EverydayStyleSection from "@/components/sellerprofile/EverydayStyleSection/index";
-import HotPicksSection from "@/components/sellerprofile/HotPicksSection/index";
-import MoreProductSection from "@/components/sellerprofile/MoreProductSection/index";
+// import SellerProfileBannerSection from "@/components/sellerprofile/SellerProfileBannerSection/index";
+// import BestDealsSection from "@/components/sellerprofile/BestDealsSection/index";
+// import CollectionSection from "@/components/sellerprofile/CollectionSection/index";
+// import EverydayStyleSection from "@/components/sellerprofile/EverydayStyleSection/index";
+// import HotPicksSection from "@/components/sellerprofile/HotPicksSection/index";
+// import MoreProductSection from "@/components/sellerprofile/MoreProductSection/index";
 
 
 const SellerProfile = () => {
     return (
         <section className="container flex flex-col gap-16">
-            <SellerProfileBannerSection />
+            {/* <SellerProfileBannerSection />
             <HotPicksSection />
             <EverydayStyleSection />
             <CollectionSection />
             <BestDealsSection />
-            <MoreProductSection />
+            <MoreProductSection /> */}
         </section>
     )
 };
