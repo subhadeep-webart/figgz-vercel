687c2fd2e7a7711e583db67ad8ca1c64c3b6afcc
subhadeep-webart <subhadeep.das@webart.technology>
2025-12-19 17:21:18 +0530

    done

--- src/app/[locale]/deals-offer/page.jsx
+++ src/app/[locale]/deals-offer/page.jsx
@@ -2,8 +2,8 @@
 // import DealsFromBrandSection from "@/components/dealsandoffer/DealsFromBrandSection";
 // import NewArrivalSection from "@/components/dealsandoffer/NewArrivalSection";
 // import ProductCatalog from "@/components/dealsandoffer/ProductCatalog";
-import DealsAndOfferBannerSection from "../../../components/dealsAndOffer/DealsAndOfferBannerSection";
-import DealsFromBrandSection from "../../../components/dealsAndOffer/DealsFromBrandSection";
+import DealsAndOfferBannerSection from "../../../components/dealsandoffer/DealsAndOfferBannerSection";
+import DealsFromBrandSection from "../../../components/dealsandoffer/DealsFromBrandSection";
 import NewArrivalSection from "../../../components/dealsandoffer/NewArrivalSection";
 import ProductCatalog from "../../../components/dealsandoffer/ProductCatalog";
 const DealsAndOffer = () => {
