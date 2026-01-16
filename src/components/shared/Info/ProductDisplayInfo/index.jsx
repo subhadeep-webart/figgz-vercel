ad3d214422e4d26e9513f1e72b66236925c9f37d
sanchita-webart <sanchita.dutta@webart.technology>
2026-01-16 15:26:53 +0530

    updated product

--- src/components/shared/Info/ProductDisplayInfo/index.jsx
+++ src/components/shared/Info/ProductDisplayInfo/index.jsx
@@ -4,14 +4,35 @@ import CustomButton from "@/components/ui/Buttons/CustomButton";
 
 const ProductDisplayInfo = () => {
   return (
-    <div className="">
-      <p className="font-normal text-text-display-info text-lg hidden lg:block">
+   <div className="">
+      <div className="lg:flex justify-between hidden">
+      <p className="font-normal text-text-display-info text-lg ">
         Showing Products{" "}
         <span className="text-foreground">
           1-12 Results for{" "}
           <span className="text-secondary font-semibold">{`"Shoe"`}</span>
         </span>
       </p>
+      <div className="flex gap-4">
+        <CustomSelect
+            SIZE_CHART={[
+              { key: "Newest", label: "Newest" },
+              { key: "Popular", label: "Popular" },
+              { key: "Most searched", label: "Most searched" },
+            ]}
+            label="Sort by:"
+          />
+
+           <CustomSelect
+            SIZE_CHART={[
+              { key: "Newest", label: "Newest" },
+              { key: "Popular", label: "Popular" },
+              { key: "Most searched", label: "Most searched" },
+            ]}
+            label="Products Per Row"
+          />
+      </div>
+      </div>
       <div className="flex justify-between lg:hidden">
         <div>
           <h3 className="font-semibold text-[#000000] text-sm">All products</h3>
--- src/components/ui/Select/index.jsx
+++ src/components/ui/Select/index.jsx
@@ -2,72 +2,24 @@
 
 import SelectCartSelectDisplay from "@/components/shared/Select/components/SelectCartSelectDisplay";
 import {
-  // Dropdown,
-  // DropdownTrigger,
-  // DropdownMenu,
-  // DropdownItem,
-  // Button,
   Select,
   SelectItem,
 } from "@heroui/react";
-// import { useState } from "react";
 
 
 
 const CustomSelect = (
   {
-    SIZE_CHART
-    // label = "Sort by",
+    SIZE_CHART,
+    label
     // defaultValue = "Newest",
     // options = ["Popular", "Most searched"],
     // onChange = () => { },
   }
 ) => {
-  // const [selected, setSelected] = useState(defaultValue);
-
-  // const handleSelect = (key) => {
-  //   setSelected(key);
-  //   onChange(key);
-  // };
 
   return (
-    // <Dropdown>
-    //   <DropdownTrigger>
-    //     <Button
-    //       radius="full"
-    //       variant="flat"
-    //       className="bg-white shadow-md border border-border-image-wrapper text-text-display-info !px-5 !py-3"
-    //       endContent={<span className="text-sm">▾</span>}
-    //     >
-    //       <span className="font-medium">{label}: </span>
-    //       <span className="ml-1 text-foreground">{selected}</span>
-    //     </Button>
-    //   </DropdownTrigger>
-
-    //   <DropdownMenu
-    //     aria-label="Sort options"
-    //     onAction={(key) => handleSelect(key)}
-    //     classNames={{
-    //       base: `
-    //                     rounded-[6px]
-    //                     border border-[#D0CDCD]
-    //                     shadow-[0_4px_4px_rgba(0,0,0,0.25)]
-    //                     bg-white
-    //                     !p-[12px]
-    //                 `,
-    //     }}
-    //   >
-    //     {options.map((item, index) => (
-    //       <DropdownItem
-    //         key={item}
-    //         className={`text-sm !px-3 !py-2 ${index !== options.length - 1 ? "border-b border-border-select-item" : ""
-    //           }`}
-    //       >
-    //         {item}
-    //       </DropdownItem>
-    //     ))}
-    //   </DropdownMenu>
-    // </Dropdown>
+   
     <Select
       items={SIZE_CHART}
       className="md:w-[184px] w-[100px]"
@@ -84,7 +36,7 @@ const CustomSelect = (
         items.map((item) => (
           <SelectCartSelectDisplay
             key={item.key}
-            label="Sort by:"
+            label={label}
             text={item.data.label}
           />
         ))
